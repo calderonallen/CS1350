c3d7b74407a55bf6de85f3b4580aafe8a0201458
calderonallen <ACalderon01@indianatech.net>
2026-04-28 12:22:06 -0400

    Update udp.server.py

--- udp.server.py
+++ udp.server.py
@@ -1,1 +1,16 @@
+import socket
 
+# Create UDP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to IP and port
+serverSocket.bind(("192.168.179.128", 9090))
+
+print("UDP server is running...")
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    print("Received from client:", message.decode())
+
+    response = "Hello back from Allen"
+    serverSocket.sendto(response.encode(), clientAddress)
